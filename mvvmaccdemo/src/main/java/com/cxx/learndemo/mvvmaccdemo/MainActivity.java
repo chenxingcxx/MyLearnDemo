fdb8639fc247579104dc0bbd0c025789c9fc5fa6
chenxing <1274605709@qq.com>
2018-12-25 15:28:48 +0800

    mvvm学习demo ...

---
+++ mvvmaccdemo/src/main/java/com/cxx/learndemo/mvvmaccdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cxx.learndemo.mvvmaccdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
